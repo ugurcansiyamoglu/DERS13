988b093429c5bf740927f177f9a8ff50d0cdf09b
ACER <ACER@DESKTOP-2QDN2A7>
2023-05-12 15:53:47 +0300

    Add project files.

---
+++ DERS13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERS13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* BİRİNCİ ÖRNEK 
+            string[] isimler = {"ali", "ayşe", "mehmet", "yakup"};
+            Console.WriteLine(isimler[2]);  // kapalı parantez içine yazılan sayıya karşılık gelen ismi ekrana yazar ve 0 dan başlar
+            Console.Read(); */
+
+            
+        }
+    }
+}
